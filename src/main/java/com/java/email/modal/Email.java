3a4273029b000c0acd2b25c01422fe0c2cf59b87
Qinyun Wang <oojbjoo@gmail.com>
2020-06-26 23:27:23 +0800

    Init project

---
+++ src/main/java/com/java/email/modal/Email.java
@@ -0,0 +1,16 @@
+package com.java.email.modal;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Email {
+    @NonNull
+    private String address;
+}
